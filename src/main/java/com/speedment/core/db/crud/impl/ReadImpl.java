b43078487bcd1df44ff27362550aa7619210a335
Emil Forslund <emil.forslund@pyknic.se>
2015-08-20 18:38:54 +0200

    Add license headers

--- src/main/java/com/speedment/core/db/crud/impl/ReadImpl.java
+++ src/main/java/com/speedment/core/db/crud/impl/ReadImpl.java
@@ -1,3 +1,19 @@
+/**
+ *
+ * Copyright (c) 2006-2015, Speedment, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); You may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.speedment.core.db.crud.impl;
 
 import com.speedment.core.config.model.Table;
@@ -22,13 +38,18 @@ public final class ReadImpl implements Read {
     private final long limit;
 
     /**
-     * ReadImpl should be constructed using the appropriate {@link Builder} class.
+     * ReadImpl should be constructed using the appropriate {@link Builder} 
+     * class.
      *
      * @param table      the table to read the entity from
      * @param selectors  the selectors used to determine which entities to read
      * @param limit      the maximum number of entities to read
      */
-    private ReadImpl(Table table, List<Selector> selectors, long limit) {
+    private ReadImpl(
+            Table table, 
+            List<Selector> selectors, 
+            long limit) {
+        
         this.table     = table;
         this.selectors = selectors;
         this.limit     = limit;
@@ -79,7 +100,8 @@ public final class ReadImpl implements Read {
         }
 
         /**
-         * Appends an additional selector to this builder to determine which entities to read.
+         * Appends an additional selector to this builder to determine which 
+         * entities to read.
          *
          * @param selector  the selector
          * @return          a reference to this builder
